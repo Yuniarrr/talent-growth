678375bf26bc51e42e791d33d486aecaf3a17504
Yuniarrr <5027211025@mhs.its.ac.id>
2023-07-19 09:58:30 +0700

    feat: all route

--- route.js
+++ route.js
@@ -9,4 +9,29 @@ router.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+// create contact
+router.post('/contact', (req, res) => {
+    console.log("create");
+});
+
+// get all
+router.get('/contacts', (req, res) => {
+    console.log("get all");
+});
+
+// get one
+router.get('/contact/:id', (req, res) => {
+    res.send("get by id");
+});
+
+// update
+router.patch('/contact/:id', (req, res) => {
+    res.send("update by id");
+});
+
+// delete
+router.delete('/contact/:id', (req, res) => {
+    res.send("delete by id");
+});
+
 export default router;
